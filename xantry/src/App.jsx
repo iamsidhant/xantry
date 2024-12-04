d3e6d7dffb83fa459a19a4216b724ac221066e5b
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-12-04 11:40:25 +0530

    project setup

---
+++ xantry/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+
+  return (
+    <>
+    <main className='relative'>
+      <h1 className='text-5xl text-orange-600 font-bold'>Welcome to a special website</h1>
+    </main>
+    </>
+  )
+}
+
+export default App
