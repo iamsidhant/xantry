9ba774ebbff7e9738197c76e2ca37e3204ba990e
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-12-04 21:10:03 +0530

    styling  and hero component added

--- xantry/src/App.jsx
+++ xantry/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Hero from './components/Hero'
 
 const App = () => {
 
   return (
     <>
-    <main className='relative'>
-      <h1 className='text-5xl text-violet-300 font-bold'>Welcome to a special website</h1>
+    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <Hero />
     </main>
     </>
   )
