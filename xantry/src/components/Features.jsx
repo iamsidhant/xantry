bce991f5290dfaad2bb07e782884147d947e5c70
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-12-18 14:07:14 +0530

    Features component added

--- xantry/src/components/Features.jsx
+++ xantry/src/components/Features.jsx
@@ -1,8 +1,57 @@
 import React from 'react'
 
+export const BentoTilt = ({ children, className = ""}) => {
+  return(
+    <div>
+      {children}
+    </div>
+  );
+};
+
+export const BentoCard = ({ src, title, description, isComingSoon}) => {
+  return(
+    <div className='relative size-full'>
+      <video
+        src={src}
+        loop
+        muted
+        autoPlay
+        className='absolute left-0 top-0 size-full object-cover object-center' 
+      />
+    </div>
+  );
+};
+
 const Features = () => {
   return (
-    <div>Features</div>
+    <section className='bg-black pb-52'>
+      <div className='container mx-auto px-3 md:px-10'>
+        <div className='px-5 py-32'>
+          <p className='font-circular-web text-lg text-blue-50'>
+            Into the Metagame Layer
+          </p>
+          <p className='max-w-md font-circular-web text-lg text-blue-50 opacity-50'>
+            Immerse yourself in a rich and ever-expanding universe where a vibrant
+            array of products converge into an interconnected overlay experience
+            on your world.
+          </p>
+        </div>
+
+        <BentoTilt className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
+          <BentoCard
+            src="videos/feature-1.mp4"
+            title={
+              <>
+                radia<b>n</b>t
+              </>
+            }
+            description="A cross-platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure."
+            isComingSoon 
+          />
+        </BentoTilt>
+
+      </div>
+    </section>
   )
 }
 
