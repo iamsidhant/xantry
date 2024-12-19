87b7167320b8c03107045f8a73287f9f5e163783
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-12-19 13:38:19 +0530

    Features section completed

--- xantry/src/components/Features.jsx
+++ xantry/src/components/Features.jsx
@@ -1,15 +1,63 @@
 import React from 'react'
+import { useState, useRef } from 'react'
 import { TiLocationArrow } from "react-icons/ti";
 
 export const BentoTilt = ({ children, className = ""}) => {
+  const [transformStyle, setTransformStyle] = useState("");
+  const itemRef = useRef(null);
+
+  const handleMouseMove = (event) => {
+    if (!itemRef.current) return;
+
+    const { left, top, width, height } = 
+      itemRef.current.getBoundingClientRect();
+
+    const relativeX = (event.clientX - left) / width;  
+    const relativeY = (event.clientY - top) / height;
+    
+    const tiltX = (relativeY - 0.5) * 5;
+    const tiltY = (relativeX - 0.5) * -5;
+
+    const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(.95, .95, .95)`;
+    setTransformStyle(newTransform);
+  };
+
+  const handleMouseLeave = () => {
+    setTransformStyle("");
+  };
+
   return(
-    <div>
+    <div
+      ref={itemRef}
+      className={className}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      style={{ transform: transformStyle }}
+    >
       {children}
     </div>
   );
 };
 
 export const BentoCard = ({ src, title, description, isComingSoon}) => {
+  const [cursorPosition, setCursorPosition] = useState({ x:0, y:0 });
+  const [hoverOpacity, setHoverOpacity] = useState(0);
+  const hoverButtonRef = useRef(null);
+
+  const handleMouseMove = (event) => {
+    if(!hoverButtonRef.current) return;
+    const rect = hoverButtonRef.current.getBoundingClientRect();
+
+    setCursorPosition({
+      x: event.clientX - rect.left,
+      y: event.clientY - rect.top,
+    });
+  };
+
+  const handleMouseEnter = () => setHoverOpacity(1);
+
+  const handleMouseLeave = () => setHoverOpacity(0);
+
   return(
     <div className='relative size-full'>
       <video
@@ -28,8 +76,24 @@ export const BentoCard = ({ src, title, description, isComingSoon}) => {
         </div>
 
         {isComingSoon && (
-          <div>
-            <div />
+          <div
+            ref={hoverButtonRef}
+            onMouseMove={handleMouseMove}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            className='border-hsla relative flex w-fit cursor-pointer items-center gap-1 
+              overflow-hidden rounded-full bg-black px-5 py-2 text-xs uppercase text-white/20'
+          >
+            {/* Radial gradient hover effect */}
+            <div
+              className='pointer-events-none absolute -inset-px opacity-0 transition duration-300'
+              style={{
+                opacity: hoverOpacity,
+                background: `radial gradient(100 px circle at ${cursorPosition.x}px ${cursorPosition.y}px, #656fe288, #00000026)`,
+              }} 
+            />
+            <TiLocationArrow className='relative z-20'/>
+            <p className='relative z-20'>coming soon</p>
           </div>
         )}
       </div>
@@ -65,7 +129,7 @@ const Features = () => {
           />
         </BentoTilt>
 
-        <div className='grid h-[135vh] w-full grid-rows-3 gap-7'>
+        <div className='grid h-[135vh] w-full grid-cols-2 grid-rows-3 gap-7'>
           <BentoTilt className='bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2'>
             <BentoCard
               src="videos/feature-2.mp4"
