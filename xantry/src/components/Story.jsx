889d25e0dae1f681348cd58cf86a72eff8b2140d
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-12-20 11:49:13 +0530

    add functionality to Story

--- xantry/src/components/Story.jsx
+++ xantry/src/components/Story.jsx
@@ -1,8 +1,51 @@
 import React from 'react'
+import gsap from 'gsap'
+import { useRef } from 'react'
+
 import Button from './Button'
 import AnimatedTitle from './AnimatedTitle'
 
 const Story = () => {
+    const frameRef = useRef(null);
+
+    const handleMouseMove = (e) => {
+        const { clientX, clientY } = e;
+        const element = frameRef.current;
+
+        if(!element) return;
+
+        const rect = element.getBoundingClientRect();
+        const xPos = clientX - rect.left;
+        const yPos = clientY - rect.top;
+
+        const centerX = rect.width / 2;
+        const centerY = rect.height / 2;
+
+        const rotateX = ((yPos - centerY) / centerY) * -10;
+        const rotateY = ((xPos - centerX) / centerX) * 10;
+
+        gsap.to(element, {
+            duration: 0.3,
+            rotateX,
+            rotateY,
+            transformPerspective: 500,
+            ease: "power1.inOut",
+        })
+    };
+
+    const handleMouseLeave = () => {
+        const element = frameRef.current;
+
+        if (element) {
+            gsap.to(element, {
+                duration: 0.3,
+                rotateX: 0,
+                rotateY: 0,
+                ease: "power1.inOut",
+            })
+        }
+    };
+
   return (
     <div id='story' className='min-h-dvh w-screen bg-black text-blue-50'>
         <div className='flex size-full flex-col items-center py-10 pb-24'>
@@ -11,18 +54,35 @@ const Story = () => {
             </p>
 
             <div className='relative size-full'>
-                {/* <AnimatedTitle /> */}
+                <AnimatedTitle
+                    title="the st<b>o<b>ry of <br /> a hidden real<b>m"
+                    containerClass="mt-5 pointer-events-none mix-blend-difference relative z-10" 
+                />
 
                 <div className='story-img-container'>
                     <div className='story-img-mask'>
                         <div className='story-img-content'>
                             <img
+                                ref={frameRef}
+                                onMouseMove={handleMouseMove}
+                                onMouseLeave={handleMouseLeave}
+                                onMouseUp={handleMouseLeave}
+                                onMouseEnter={handleMouseLeave}
                                 src='/images/entrance.webp'
                                 alt='entrance.webp'
                                 className='object-contain' 
                             />
                         </div>
                     </div>
+
+                    {/* for the rounded corner */}
+                    <svg
+                        className='invisible absolute size-0'
+                    >
+                        <defs>
+                            <filter id='flt_tag'></filter>
+                        </defs>
+                    </svg>
                 </div>
             </div>
 
@@ -34,7 +94,11 @@ const Story = () => {
                         opportunities.
                     </p>
 
-                    {/* <Button /> */}
+                    <Button
+                        id="realm-btn"
+                        title="discover prologue"
+                        containerClass="mt-5" 
+                    />
                 </div>
             </div>
         </div>
